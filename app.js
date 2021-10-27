9d63a0385c7d3e3740c1c7a1bcfc88a79677e0bf
Baptiste <74823118+baptisteCanac@users.noreply.github.com>
2021-10-27 19:24:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+/* randomize the master password */
+const masterPswFirstLetter = letters[Math.floor(Math.random() * letters.length)]
+const masterPswSecondLetter = letters[Math.floor(Math.random() * letters.length)]
+const masterPswThirdLetter = letters[Math.floor(Math.random() * letters.length)]
+const masterPswFourthLetter = letters[Math.floor(Math.random() * letters.length)]
+const masterPswFifthLetter = letters[Math.floor(Math.random() * letters.length)]
+
+let masterPassword = masterPswFirstLetter + masterPswSecondLetter + masterPswThirdLetter + masterPswFourthLetter + masterPswFifthLetter
+
+function enterButton(){
+	let passwordEnter = document.getElementById("psw").value;
+	if (passwordEnter == masterPassword){
+		window.open("https://google.com")
+	}
+	else{
+		console.log("wrong password")
+		document.getElementById("wrongPassword").innerHTML = "wrong password";
+	}
+}
